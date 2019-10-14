2481ce7a2dd06d40c24d8d4fbf6f0329e62d98fc
omergal99 <omergal99gm@gmail.com>
2019-10-14 07:34:24 +0300

    Updates

---
+++ precache-manifest.65e1e7a67cb91c51e3609672ffdb4a61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25ada43a0a0d6c7c21c13fcf4cfc603",
+    "url": "/device-manager/index.html"
+  },
+  {
+    "revision": "ad4103cec11d9221d411",
+    "url": "/device-manager/static/css/main.a96435ab.chunk.css"
+  },
+  {
+    "revision": "12b468ba70e71d9ded5a",
+    "url": "/device-manager/static/js/2.9552cbf8.chunk.js"
+  },
+  {
+    "revision": "ad4103cec11d9221d411",
+    "url": "/device-manager/static/js/main.07033774.chunk.js"
+  },
+  {
+    "revision": "591dbf152758d3747ca4",
+    "url": "/device-manager/static/js/runtime-main.8674e9af.js"
+  },
+  {
+    "revision": "7e0ee8c45d872446649aacd3ce96e1f7",
+    "url": "/device-manager/static/media/SourceSansPro-Bold.7e0ee8c4.ttf"
+  },
+  {
+    "revision": "3ba9836fde035d78b4c3f58db4a763ba",
+    "url": "/device-manager/static/media/SourceSansPro-Light.3ba9836f.ttf"
+  },
+  {
+    "revision": "3f482321becfaa40fcb19825fa717859",
+    "url": "/device-manager/static/media/SourceSansPro-Regular.3f482321.ttf"
+  }
+]);
