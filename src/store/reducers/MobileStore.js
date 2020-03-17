7602797581885d844894cc88c3d19c20777ad331
omergal99 <omergal99gm@gmail.com>
2020-03-17 22:21:26 +0200

    makeDeepCopy

--- src/store/reducers/MobileStore.js
+++ src/store/reducers/MobileStore.js
@@ -7,16 +7,19 @@ import {
 const initialState = null;
 
 export default (state = initialState, action) => {
-  var copy = makeDeepCopy(state);
+  // let copy = makeDeepCopy(state);
+  let copy = { ...state };
   switch (action.type) {
     case SET_DEVICE_DATA:
       return action.payload;
     case SET_CURR_DEVICE_BY_ID:
+      console.log('set curr');
       copy.list = copy.list.map(dev => copy.currDevice && dev._id === copy.currDevice._id ? copy.currDevice : dev);
       const deviceById = copy.list.find(dev => dev._id === action.payload);
       copy.currDevice = deviceById;
       return copy;
     case UPDATE_CURR_DEVICE:
+      console.log('update');
       copy.currDevice = action.payload;
       return copy;
     default:
@@ -24,6 +27,6 @@ export default (state = initialState, action) => {
   }
 }
 
-function makeDeepCopy(state) {
-  return JSON.parse(JSON.stringify(state));
-}
+// function makeDeepCopy(state) {
+//   return JSON.parse(JSON.stringify(state));
+// }
